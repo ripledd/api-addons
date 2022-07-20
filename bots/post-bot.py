3dc6a4a6d28ef4c8ae43c080450ca00098249dfd
SpeecherTeam <105486423+SpeecherTeam@users.noreply.github.com>
2022-07-20 23:14:49 +0300

    Update post-bot.py

--- bots/post-bot.py
+++ bots/post-bot.py
@@ -13,7 +13,11 @@ c = pycurl.Curl()
 c.setopt(c.URL, 'https://speecher.me/dev/api/post.php')
 
 # Important!(set values and configure account's log in details, email and password with sha256 encryption.)
-post_data = {'spchr_auth_email': 'examplemail@mymail.com', 'spchr_auth_pw_hash': 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'send_data': comp_data}
+post_data = 
+{'spchr_auth_email': 'examplemail@mymail.com', 
+ 'spchr_auth_pw_hash': 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 
+ 'send_data': comp_data
+}
 # Important!
 
 postfields = urlencode(post_data)
